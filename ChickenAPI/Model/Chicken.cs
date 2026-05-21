35cb35f9706baa4760cfb643fedca8165748f3c7
Reece Wanvig <rwanvig@emeris.ac.za>
2026-05-21 10:34:21 +0200

    Make Name and Breed properties nullable in Chicken model

--- ChickenAPI/Model/Chicken.cs
+++ ChickenAPI/Model/Chicken.cs
@@ -10,11 +10,11 @@ namespace ChickenAPI.Model
 
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
         [MaxLength(50)]
-        public string Breed { get; set; }
+        public string? Breed { get; set; }
 
         [Range(0, 50)] // Assuming chickens won’t live past 50 years
         public int Age { get; set; }
