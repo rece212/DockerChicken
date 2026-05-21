0a66489278a1f830f97bd91b08f62c5547ba447a
Reece Wanvig <rwanvig@emeris.ac.za>
2026-05-21 10:21:24 +0200

    Refactor HomeController to fetch chicken data from updated API endpoint; enhance Index view with new layout and styling

--- ChickMVC/Controllers/HomeController.cs
+++ ChickMVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ChickMVC.Controllers
         {
             try
             {
-                var response = await _httpClient.GetStringAsync("http://api-service:8080/GetMeEggs"); // Await the call
+                var response = await _httpClient.GetStringAsync("http://api-service:8080/api/chicken"); // Await the call
                 ViewBag.Chicken = response;
             }
             catch (Exception ex)
